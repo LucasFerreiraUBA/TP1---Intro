7ec4952e411d0d700f4dec55754e2f7bc441eab8
Chaile-mendez <chailemendez.juan@gmail.com>
2024-06-11 19:30:32 -0300

    modelo inicial

---
+++ models.py
@@ -0,0 +1,21 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Empleado(db.Model):
+    __tablename__ = 'empleados'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    apellido = db.Column(db.String(255), nullable=False)
+    dni = db.Column(db.Integer, primary_key = True)
+    horario_entrada = db.Column(db.Time, nullable=False)
+    horario_salida = db.Column(db.Time, nullable=False)
+    
+class Registro(db.Model):
+    __tablename__ = 'registros'
+    id = db.Column()
+    horario = db.Column(db.DateTime, nullable=False)
+    empleado_id = db.Column(db.Integer, db.ForeignKey('empleados.id'))
+    es_entrada = db.Column(db.Boolean, nullable = False, default = True)
+    desfase = db.Column(db.Time, nullable= False)
