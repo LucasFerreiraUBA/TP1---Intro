39739e1f098fa708b3939f4d98e6c22ee002095e
Chaile-mendez <chailemendez.juan@gmail.com>
2024-06-11 23:14:26 -0300

    Cambios menores y rutas para el front agregadas

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ port = 5000
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/tpdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#Constantes
+# Constantes
 MAXIMO_REGISTROS_POR_DEFECTO = 10
 
 
-#Front
-@app.route('/')
+# Front
+@app.route('/', methods=['GET'])
 def indice():
     return render_template('index.html')
 
-@app.route('/empleados')
+
+@app.route('/empleados', methods=['GET'])
 def empleados():
     empleados = [
         {
@@ -29,56 +30,76 @@ def empleados():
     ]
     return render_template('empleados/empleados.html', empleados=empleados)
 
-@app.route('/empleados/<int:id>')
+
+@app.route('/empleados/<int:id>', methods=['GET'])
 def empleado(id):
     empleado = {
         'nombre': 'juan',
-        'id' : id
+        'id': id
     }
     return render_template('empleados/empleado.html', empleado=empleado)
 
-#Endpoints Registros
-@app.route('api/v1/registros', methods=['GET'])
+
+@app.route('/registros/agregar', methods=['GET'])
+def front_agregar_registro():
+    return render_template('registros/nuevo_registro.html')
+
+@app.route('/registros', methods=['GET'])
+def front_agregar_registro():
+    return render_template('registros/nuevo_registro.html')
+
+# Endpoints Registros
+@app.route('/api/v1/registros', methods=['GET'])
 def obtener_registros():
     return 'todos los registros'
 
-@app.route('api/v1/registros/<int:id>', methods=['GET'])
+
+@app.route('/api/v1/registros/<int:id>', methods=['GET'])
 def obtener_registro(id):
     return 'un registro segun id'
 
-@app.route('api/v1/registros', methods=['POST'])
+
+@app.route('/api/v1/registros', methods=['POST'])
 def agregar_registro(request):
     return 'agregar un registro'
-    
-@app.route('api/v1/registros/<int:id>', methods=['DELETE'])
+
+
+@app.route('/api/v1/registros/<int:id>', methods=['DELETE'])
 def eliminar_registro(id):
     return 'eliminar un registro'
 
-@app.route('api/v1/registros/<int:id>', methods=['PUT'])
+
+@app.route('/api/v1/registros/<int:id>', methods=['PUT'])
 def agregar_registro(id):
     return 'actualizar un registro'
 
-#Endpoints Empleados
+# Endpoints Empleados
 
-@app.route('api/v1/empleados/<int:id>', methods=['GET'])
+
+@app.route('/api/v1/empleados/<int:id>', methods=['GET'])
 def obtener_empleado(id):
     return 'un empleado'
 
-@app.route('api/v1/empleados',methods=['GET'])
+
+@app.route('/api/v1/empleados', methods=['GET'])
 def obtener_empleados():
     return 'todos los empleados'
 
-@app.rotue('api/v1/empleados', methods=['POST'])
+
+@app.rotue('/api/v1/empleados', methods=['POST'])
 def agregar_empleado():
     return 'agregar empleado'
 
-@app.rotue('api/v1/empleados/<int:id>', methods=['DELETE'])
+
+@app.rotue('/api/v1/empleados/<int:id>', methods=['DELETE'])
 def eliminar_empleado():
     return 'eliminar empleado'
 
-@app.route('api/v1/empleados/<int:id>', methods=['PUT'])
+
+@app.route('/api/v1/empleados/<int:id>', methods=['PUT'])
 def actualizar_empleado(id):
     return 'actualizar empleado'
+
 
 db.init_app(app)
 
