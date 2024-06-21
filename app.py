316854d9fc5523f0937b5c3b431aaa2956ef8612
Jose Luis Juan <jjarro@fi.uba.ar>
2024-06-21 14:19:18 -0300

    agrego detalles en editar empleado

--- app.py
+++ app.py
@@ -293,10 +293,6 @@ def actualizar_empleado(id):
 
 db.init_app(app)
 
-with app.app_context():
-        db.drop_all()
-        db.create_all()
-
 if __name__ == '__main__':
     
     with app.app_context():
